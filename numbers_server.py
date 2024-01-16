0a727a6d57e0cb50b863fd8004154d651bce433c
beckermana <116715897+AlexanderBeckerman@users.noreply.github.com>
2024-01-16 13:44:02 +0200

    added file read

--- numbers_server.py
+++ numbers_server.py
@@ -5,6 +5,7 @@ def main():
 
     args = sys.argv
     port = 1337
+
     if len(args) != 2 or len(args) != 3:
         print("error in args count")
         exit(1)
@@ -13,3 +14,13 @@ def main():
         port = args[2]
     user_file_path = args[1]
 
+    user_dict = {}
+    with open(user_file_path, 'r') as file:
+        for line in file:
+            # Split each line into username and password using tab as the delimiter
+            username, password = line.strip().split('\t')
+
+            # Add the username and password to the dictionary
+            user_dict[username] = password
+
+    print(user_dict)
