cff5bd84aa9c6edf25043efad7afe70f2cae6664
beckermana <116715897+AlexanderBeckerman@users.noreply.github.com>
2024-01-16 13:38:38 +0200

    added users file

--- numbers_server.py
+++ numbers_server.py
@@ -1,1 +1,15 @@
-print("hello world")
+from socket import *
+import sys
+
+def main():
+
+    args = sys.argv
+    port = 1337
+    if len(args) != 2 or len(args) != 3:
+        print("error in args count")
+        exit(1)
+
+    if len(args) == 3:
+        port = args[2]
+    user_file_path = args[1]
+
