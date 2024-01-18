4b32154ed8dc6781b31725496df761217f7e5304
beckermana <116715897+AlexanderBeckerman@users.noreply.github.com>
2024-01-18 15:17:20 +0200

    working on server

--- numbers_server.py
+++ numbers_server.py
@@ -41,11 +41,28 @@ def main():
                 init_new_sock(sockets_data, sock)
                 welcome_msg = "Welcome! Please log in"
                 bytes_sent = sendall(client_socket, welcome_msg.encode('utf-8'))
-                if bytes_sent < len(welcome_msg.encode('utf-8')):
-                    handle_error(client_socket)
-                    connected_clients.remove(client_socket)
-                    sockets_data.pop(client_socket)
+                # if bytes_sent < len(welcome_msg.encode('utf-8')):
+                #     handle_error(client_socket)
+                #     connected_clients.remove(client_socket)
+                #     sockets_data.pop(client_socket)
             else:
+                if sockets_data[sock]["logged"] == 0:
+                    # in the client make sure to send twice AFTER getting input for both user and pass
+                    username = sock.recv(4096).decode('utf-8').split(":") # change later to recv all
+                    password = sock.recv(4096).decode('utf-8').split(":")
+                    if username[0] != "User" or password[0] != "Password":
+                        handle_error(sock, connected_clients)
+
+                    if check_credentials(username[1].strip(), password[1].strip(), user_dict): # successful login
+                        success_msg = f"Hi {username[1].strip}, good to see you"
+                        bytes_sent = sendall(sock, success_msg.encode('utf-8'))
+                        sockets_data[sock]["logged"] = 1
+
+                    else:
+                        fail_msg = "Failed to login"
+                        bytes_sent = sendall(sock, fail_msg.encode('utf-8'))
+                else: # user is logged in and we can receive commands
+
 
 
 def check_credentials(enteredUser, enteredPass, user_dict):
@@ -59,9 +76,7 @@ def check_credentials(enteredUser, enteredPass, user_dict):
 
 def init_new_sock(sockets_data, sock):
     sockets_data[sock] = {}
-    sockets_data[sock]["username"] = ""
-    sockets_data[sock]["password"] = ""
-    sockets_data[sock]["commend"] = ""
+    sockets_data[sock]["command"] = ""
     sockets_data[sock]["logged"] = 0
     sockets_data[sock]["welcomed"] = 0
 
@@ -84,9 +99,13 @@ def recvall(sock):
     return
 
 
-def handle_error(sock):
+def handle_error(sock, connected_clients):
     print("Error sending message!, closing client socket")
+    connected_clients.remove(sock)
     sock.close()
 
+
+def handle_login(socket, username, password, user_dict):
+    flag = check_credentials(username, password, user_dict)
 
 main()
